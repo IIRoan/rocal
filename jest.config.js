d349401e3d73a048763c4b3d4e369b5cedc793e4
IIRoan <github.com.q6c0y@lunary.roan.zip>
2026-03-27 00:00:45 +0100

    fix(test): configure test environment for react-native

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   preset: 'react-native',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)/)',
+    'node_modules/(?!react-native|@react-native|@react-native/js-polyfills)',
   ],
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)',
+  ],
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@react-native/js-polyfills/error-guard$': '<rootDir>/jest/mocks/error-guard.js',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 };
