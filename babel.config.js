adc818bd4287761b8333575de813ef357fe44e5b
IIRoan <github.com.q6c0y@lunary.roan.zip>
2026-03-27 15:04:05 +0100

    feat(scrutiny): add synthesis report for Calendar Swipe Controls

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,15 @@ module.exports = function (api) {
   const isTest = api.env('test');
   const presets = [
     'module:metro-react-native-babel-preset',
+    isTest && [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    '@babel/preset-typescript',
     isTest && 'babel-preset-jest',
   ].filter(Boolean);
 
