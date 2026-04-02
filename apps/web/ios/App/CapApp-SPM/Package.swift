2ffb88cec1a73d8621b063aab1e240efcc3471b6
IIRoan <github.com.q6c0y@lunary.roan.zip>
2026-04-02 18:05:03 +0200

    Add build

--- apps/web/ios/App/CapApp-SPM/Package.swift
+++ apps/web/ios/App/CapApp-SPM/Package.swift
@@ -11,11 +11,12 @@ let package = Package(
             targets: ["CapApp-SPM"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.2.0"),
-        .package(name: "CapacitorApp", path: "..\..\..\..\..\node_modules\.bun\@capacitor+app@8.0.1+1a4bec108d739029\node_modules\@capacitor\app"),
-        .package(name: "CapacitorHaptics", path: "..\..\..\..\..\node_modules\.bun\@capacitor+haptics@8.0.1+1a4bec108d739029\node_modules\@capacitor\haptics"),
-        .package(name: "CapacitorKeyboard", path: "..\..\..\..\..\node_modules\.bun\@capacitor+keyboard@8.0.1+1a4bec108d739029\node_modules\@capacitor\keyboard"),
-        .package(name: "CapacitorStatusBar", path: "..\..\..\..\..\node_modules\.bun\@capacitor+status-bar@8.0.1+1a4bec108d739029\node_modules\@capacitor\status-bar")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.3.0"),
+        .package(name: "CapacitorApp", path: "..\..\..\..\..\node_modules\.bun\@capacitor+app@8.1.0+db0c1b46371b240c\node_modules\@capacitor\app"),
+        .package(name: "CapacitorBrowser", path: "..\..\..\..\..\node_modules\.bun\@capacitor+browser@8.0.3+db0c1b46371b240c\node_modules\@capacitor\browser"),
+        .package(name: "CapacitorHaptics", path: "..\..\..\..\..\node_modules\.bun\@capacitor+haptics@8.0.1+db0c1b46371b240c\node_modules\@capacitor\haptics"),
+        .package(name: "CapacitorKeyboard", path: "..\..\..\..\..\node_modules\.bun\@capacitor+keyboard@8.0.2+db0c1b46371b240c\node_modules\@capacitor\keyboard"),
+        .package(name: "CapacitorStatusBar", path: "..\..\..\..\..\node_modules\.bun\@capacitor+status-bar@8.0.2+db0c1b46371b240c\node_modules\@capacitor\status-bar")
     ],
     targets: [
         .target(
@@ -24,6 +25,7 @@ let package = Package(
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm"),
                 .product(name: "CapacitorApp", package: "CapacitorApp"),
+                .product(name: "CapacitorBrowser", package: "CapacitorBrowser"),
                 .product(name: "CapacitorHaptics", package: "CapacitorHaptics"),
                 .product(name: "CapacitorKeyboard", package: "CapacitorKeyboard"),
                 .product(name: "CapacitorStatusBar", package: "CapacitorStatusBar")
