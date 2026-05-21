c3cb8cffa8020d71a0777ce7cf2cad61211a4438
RoanVW <roan.van.westerop@test-correct.nl>
2026-05-21 11:34:09 +0200

    revert metro to js

--- apps/native/metro.config.ts
+++
@@ -1,8 +0,0 @@
-import { getDefaultConfig } from "expo/metro-config";
-import type { MetroConfig } from "expo/metro-config";
-
-const config: MetroConfig = getDefaultConfig(
-  new URL(".", import.meta.url).pathname,
-);
-
-export default config;
